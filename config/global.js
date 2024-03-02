66c9cda01e0bc6abec9586e1312ba86e86afe929
coder4al <coder4al@gmail.com>
2024-03-02 23:53:09 +0530

    first commit

---
+++ config/global.js
@@ -0,0 +1,3 @@
+const API_URL = "https://trackzi-server.onrender.com";
+
+export default API_URL;
---
+++ src/redux/taskSlice.js
@@ -0,0 +1,125 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+import API_URL from "../../config/global";
+
+const initalTask = localStorage.getItem("task")
+  ? JSON.parse(localStorage.getItem("task"))
+  : null;
+
+const initialState = {
+  TaskData: initalTask,
+  AllTasks: {},
+};
+export const taskSlice = createSlice({
+  name: "Task",
+  initialState,
+
+  reducers: {
+    taskAddedSuccessfully: (state, action) => {
+      state.TaskData = action.payload;
+    },
+    taskAddFailure: (state) => {
+      return state;
+    },
+    getAllTaskSuccess: (state, action) => {
+      state.AllTasks = action.payload;
+    },
+    getAllTaskFailure: (state) => {
+      return state;
+    },
+
+    editTaskSuccess: (state, action) => {
+      state.TaskData = action.payload;
+    },
+
+    deleteSuccess: (state, action) => {
+      state.TaskData = action.payload;
+    },
+    deletefail: (state) => {
+      return state;
+    },
+  },
+});
+
+export const {
+  taskAddFailure,
+  taskAddedSuccessfully,
+  getAllTaskFailure,
+  getAllTaskSuccess,
+  deleteSuccess,
+  deletefail,
+  editTaskSuccess,
+} = taskSlice.actions;
+
+export default taskSlice.reducer;
+
+export const addTask = (task, id) => async (dispatch) => {
+  const taskData = {
+    task,
+    id,
+  };
+  const response = await axios.post(`${API_URL}/task/add`, taskData);
+  if (response) {
+    localStorage.setItem("task", JSON.stringify(response.data));
+
+    dispatch(taskAddedSuccessfully(response.data));
+    toast.success("task added successfully");
+    window.location.reload();
+  } else {
+    dispatch(taskAddFailure());
+  }
+};
+
+export const getAllTasks = (token, id) => async (dispatch) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      id,
+    },
+  };
+
+  try {
+    const response = await axios.get(`${API_URL}/task/tasks`, config);
+    if (response) {
+      dispatch(getAllTaskSuccess(response.data));
+    }
+  } catch (error) {
+    if (error.response.status === 400) {
+      dispatch(getAllTaskFailure());
+    }
+  }
+};
+
+export const arrowClick = (item, string) => async () => {
+  let taskData = {
+    id: item._id,
+    status: item.status,
+    string,
+  };
+
+  try {
+    let response = await axios.put(`${API_URL}/task/${taskData.id}`, taskData);
+
+    if (response) {
+      window.location.reload();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteItem = (id) => async (dispatch) => {
+  let res = await axios.delete(`${API_URL}/task/${id}`);
+
+  if (res) {
+    dispatch(deleteSuccess());
+    toast.success("task deleted successfully");
+
+    window.location.reload();
+  } else {
+    dispatch(deletefail());
+  }
+};
